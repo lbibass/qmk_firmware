4411123a7c18579da13f9d99af391e3d180d390d
Lbibass <ewdries02@gmail.com>
2020-07-06 17:51:47 -0400

    added more via layers

--- keyboards/cannonkeys/an_c/keymaps/lbibass_tsangan/config.h
+++ keyboards/cannonkeys/an_c/keymaps/lbibass_tsangan/config.h
@@ -1,2 +1,3 @@
-#pragma once 
-
+#pragma once
+#define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 3071 // this lets you use more of the 8kb of eeprom availible on the STM32F072CBT6. (3kB here)
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
