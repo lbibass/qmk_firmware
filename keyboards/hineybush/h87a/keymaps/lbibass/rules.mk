2c66c1a57ca748ce76221e6b0ac05bc8572b9e82
lbibass <ewdries02@gmail.com>
2021-07-28 14:08:03 -0400

    Revised link Time optimization to compile.

--- keyboards/hineybush/h87a/keymaps/lbibass/rules.mk
+++ keyboards/hineybush/h87a/keymaps/lbibass/rules.mk
@@ -5,4 +5,4 @@ COMMAND_ENABLE = yes        # Commands for debug and configuration
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality on B7 by default
 RGBLIGHT_ENABLE = no       # Enable RGB underglow
 MOUSEKEY_ENABLE = no       # Mouse keys
-LINK_TIME_OPTIMIZATION_ENABLE = yes
+LTO_ENABLE = on
