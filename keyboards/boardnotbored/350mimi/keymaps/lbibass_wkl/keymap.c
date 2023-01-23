72b36726df9f9482c09a840d4d2e058f0b01ab41
Lbibass <notlbibass@gmail.com>
2023-01-23 15:49:36 -0500

    I lied, SPI no worky. Bitbang work though.

--- keyboards/boardnotbored/350mimi/keymaps/lbibass_wkl/keymap.c
+++ keyboards/boardnotbored/350mimi/keymaps/lbibass_wkl/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 3: fn */
   LAYOUT_60_ansi_tsangan(
     KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_DEL,
-    KC_CAPS, _______, _______,   _______, RESET, _______, _______, _______, _______, _______, _______, KC_UP,   _______, KC_BSLS,
+    KC_CAPS, RGB_TOG, RGB_MODE_REVERSE,   RGB_MODE_FORWARD, RESET, _______, _______, _______, _______, _______, _______, KC_UP,   _______, KC_BSLS,
     _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, _______, _______, _______, KC_LEFT, KC_RGHT, _______,
     _______, _______, _______, _______, _______, HPT_TOG,HPT_DWLI,HPT_DWLD, _______, _______, KC_DOWN, _______,
     _______, _______, _______,                    _______,                  _______, _______,  _______
